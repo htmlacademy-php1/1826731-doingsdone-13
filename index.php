c954151db414f7d6aed6ed8704d8ae9b7fdb862f
Shishkeks <shishalov_yura@mail.ru>
2021-08-24 00:30:40 +0300

    Работа с датами

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $tasks = [
     ], 
     [
         'title' => 'Выполнить тестовое задание',
-        'date' => '25.12.2019',
+        'date' => '25.12.2021',                       
         'category' => 'Работа',
         'status' => false
     ],
@@ -37,7 +37,7 @@ $tasks = [
         'status' => false
     ],
 ];
-function task_count($task_array, $category )
+function task_count($task_array, $category )     //Функция подсчета задач
 {
     $count = 0;
     foreach ($task_array as $task) {
@@ -47,7 +47,7 @@ function task_count($task_array, $category )
     }
     return $count;
 };
-function include_template($name, array $data = []) {
+function include_template($name, array $data = []) {    //Функция подключения шаблона
     $name = 'templates/' . $name;
     $result = '';
 
@@ -63,7 +63,22 @@ function include_template($name, array $data = []) {
 
     return $result;
 };
+function diff_btw_dates($array_date) {           //Функция подсчета разницы между датами
+
+    $current_date = strtotime("now");           //Задаём текущую дату
+    $task_date = strtotime($array_date);        //Берём дату из массива 
+    $calc_date = $current_date - $task_date;    //Вычитаем разницу между датами
+        if ($array_date == 'null') {            //Не учитываем задачи с датой 'null'
+            return false;
+        }
+    $result = floor($calc_date/3600);           //Приводим дату к целому числу и переводим в часы
+
+   return $result;
+};
+//Основное содержимое страницы
 $page_content = include_template('main.php', ['categories' => $categories, 'tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]); 
+//Вся страница
 $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => $title]);
+//Вывод страницы
 print($layout_content);
 ?>
